4b2293d4f2e56c67b70d75c2ff76d797a86451aa
ilianapeneva <iliana.s.peneva@gmail.com>
2018-05-30 18:27:55 +0100

    pick the 1000 differentially expressed genes and run BHC

--- src/pvalues.R
+++ src/pvalues.R
@@ -1,17 +1,21 @@
+
+# perform t-test to select the genes
 p <- rep(0,length(rownames(tumor)))
 for (i in 1:length(rownames(tumor))) {
   p[i] <- t.test((tumor[i,]),(normal[i,]))$p.value
 }
 
 tumor$pvalues <-  p
+# need to do Bonferroni correction!!!!
 tumor.ordered <- tumor[order(tumor$pvalues,decreasing = F),]
 
-
+# select only the data that corresponds to the 1000 genes
 data <- as.matrix(na.omit(tumor.ordered[1:1000,1:(ncol(tumor.ordered)-1)]))
 nDataItems <- nrow(data)
 nFeatures <- ncol(data)
 itemLabels <- rownames(data)
 
+# set up to use Bayesian Hierarchical Clustering (BHC) code
 percentiles <- FindOptimalBinning(data,itemLabels,transposeData = T,verbose = T)
 discreteData <- DiscretiseData(t(data),percentiles = percentiles)
 discreteData <- t(discreteData)
