711c0a4691726c5467cd8ad3e5dd8db8dbeaacf5
ilianapeneva <iliana.s.peneva@gmail.com>
2018-05-30 18:14:25 +0100

    Update KM.R

--- src/KM.R
+++ src/KM.R
@@ -18,16 +18,7 @@ s <- Surv(data.bio$days_to_last_followup, as.numeric(as.character(data.bio$vital
 # vital_status is a binary var; 1 = deceased, 0 = living
 # days to last follow up = days to death/days to follow up
 
-model <- coxph(s ~ data.bio$ajcc_tumor_stage_code, na.action = na.exclude)
-# fits a Cox prop. hazards regression model
-
-summary(model)
-cox.zph(model)
-# test the assumption for proportionality - need to get appr. straight lines
-
-plot(cox.zph(model))
-# plot the result
-
+# example: compare the survival outcomes of men and women;
+#  testing the null hypothesis that the survivals of the 2 groups are from the same distribution 
 fit.bygender <- survfit(Surv(days_to_last_followup, vital_status == 1) ~ gender, data = data.bio)
 plot(fit.bygender, conf.int = TRUE, col = c("black", "grey"), lty = 1:2, legend.text=c("Female", "Male"))
-# comparison of the KM survival curves for men and women
