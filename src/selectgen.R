a2e0c9b24c8df0a8c42e244d406fd5e60f79c122
ilianapeneva <iliana.s.peneva@gmail.com>
2018-05-30 18:37:55 +0100

    pick most relevant genes and run BHC on patients and genes

--- src/selectgen.R
+++ src/selectgen.R
@@ -1,3 +1,5 @@
+# R script to run Bayesian Hierarchical Clustering (BHC) with the most relevant genes
+
 # only relevant genes
 
 genelist <- genes$gene[1:500]
@@ -6,6 +8,7 @@ subset <- sapply(genelist,grep,rownames(cnsdf))
 subset <- unique(rapply(subset,function(x) head(x,1)))
 cnsdf[subset,]
 
+# set up the colours for the heatmap
 colors = c(seq(-2,-0.4,length=33),seq(-0.4,0.4,length=35),seq(0.4,2,length=33))
 #heatmap.2(as.matrix(cnsdf[subset,]),trace="none",hclustfun = function(x) hclust(x,"ave"),col=redgreen(100),breaks=colors)
 
@@ -15,6 +18,7 @@ nDataItems <- nrow(data)
 nFeatures <- ncol(data)
 itemLabels <- rownames(data)
 
+# run BHC for patients
 percentiles <- FindOptimalBinning(data,itemLabels,transposeData = T,verbose = T)
 discreteData <- DiscretiseData(t(data),percentiles = percentiles)
 discreteData <- t(discreteData)
@@ -25,7 +29,8 @@ data <- t(data) #this is not the transpose
 nDataItems <- nrow(data)
 nFeatures <- ncol(data)
 itemLabels <- rownames(data)
-
+                        
+# run BHC for genes
 percentiles <- FindOptimalBinning(data,itemLabels,transposeData = T,verbose = T)
 discreteData <- DiscretiseData(t(data),percentiles = percentiles)
 discreteData <- t(discreteData)
