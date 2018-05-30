24217004622a676443db414a8ed8273becd5bd00
ilianapeneva <iliana.s.peneva@gmail.com>
2018-05-30 18:21:05 +0100

    explore heatmaps

--- src/heatmap_intro.R
+++ src/heatmap_intro.R
@@ -1,22 +1,23 @@
-# Read in correct file
-heatmap <- synGet("syn1571514") # can do download = false
-heatmap@filePath
-# [1] "C:/Users/Esther Wershof/Documents/.synapseCache/519/2099519/broad.mit.edu_BLCA_Genome_Wide_SNP_6.hg19.seg.hugo.whitelist_tumor"
-data.heatmap <- read.delim(heatmap@filePath,header=TRUE)
+# load the necessary libraries
 require(gplots)
 
+# Read in correct file
+heatmap <- synGet("syn1571514") # can do download = false
+data.heatmap <- read.delim(heatmap@filePath,header=TRUE)
+
+# pick the first 1000 observations 
 subset <- data.heatmap[1:1000,]
 row.names(subset) <- subset$probe
 subset <- subset[,-1]
 
-# create heatmap
+# create heatmap and play with what to display
 heatmap.2(as.matrix(subset))
 heatmap.2(as.matrix(subset),trace="none")
 heatmap.2(as.matrix(subset),trace="none",col=redgreen)
 heatmap.2(as.matrix(subset),trace="none",col=redgreen(100))
 heatmap.2(as.matrix(subset),trace="none",col=redgreen(100),hclustfun = function(x) hclust(x,"ave"))
 
-# look at dendograms
+# look at dendograms we get from hierarchical clustering
 d <- dist(t(subset))
 hc1 <- hclust(d,"ave") # average distance
 plot(hc1)
@@ -29,5 +30,3 @@ plot(hc1,hang = -1)
 # clustering according to another dendogram
 heatmap.2(as.matrix(subset),trace="none",col=redgreen(100),hclustfun = function(x) hclust(x,"ave"),Colv = as.dendrogram(hc1))
 View(data.heatmap)
-
-# best way to zoom in on image is to export it as pdf
