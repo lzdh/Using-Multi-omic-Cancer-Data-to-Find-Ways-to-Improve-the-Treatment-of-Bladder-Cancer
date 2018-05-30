a52e078ca2fe5ea4920d345ac372199673cc62de
ilianapeneva <iliana.s.peneva@gmail.com>
2018-05-30 18:16:57 +0100

    how to login with R on Synapse to access TCGA data

--- src/init.R
+++ src/init.R
@@ -1,5 +1,11 @@
+# script to login on Synapse in R (can access TCGA data without having to download it)
+# need to create an account at Synapse first
+
+
 require(synapseClient)
 require(gplots)
 require(BHC)
 
 synapseLogin()
+
+# enter username and password
